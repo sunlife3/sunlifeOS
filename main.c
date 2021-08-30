b4d22827f6ae9cdd94a20b37a466f5dbf0cdc428
sunlife3 <43718880+sunlife3@users.noreply.github.com>
2021-08-30 22:45:40 +0900

    add GetMemoryMap

--- main.c
+++ main.c
@@ -5,13 +5,17 @@
 EFI_SYSTEM_TABLE *gST;
 EFI_BOOT_SERVICES *gBS;
 
+void Halt(void){
+    while (1) __asm__("hlt");
+}
 
-EFI_STATUS GetMemoryMap(struct MemoryMap* map){
+
+EFI_STATUS GetMemoryMap(struct MemoryMap* map,EFI_SYSTEM_TABLE *SystemTable){ 
     if(map->buffer == 0){
         return 1;
     }
     map->map_size = map->buffer_size;
-    return gBS->GetMemoryMap(
+    return SystemTable->BootServices->GetMemoryMap(
         &map->map_size,
         (EFI_MEMORY_DESCRIPTOR*)map -> buffer,
         &map->map_key,
@@ -19,19 +23,22 @@ EFI_STATUS GetMemoryMap(struct MemoryMap* map){
         &map->descriptor_version);
 }
 
+CHAR8 memmap_buf[4096*4];
 
 EFI_STATUS EfiMain (EFI_HANDLE ImageHandle,
                     EFI_SYSTEM_TABLE *SystemTable){
     EFI_STATUS status;
-
+    
     SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
     SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello UEFI!\n");
     
-    //char memmap_buf[8000];
-    //int a[1];
-    //struct nMemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
-    //status = GetMemoryMap(&memmap);
-
+    struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
+    status = GetMemoryMap(&memmap,SystemTable);
+    if(status){
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Failed to open root directory\n");
+        Halt();
+    }
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"succcess open root directory\n");
     while(1);
     return EFI_SUCCESS;
 }
