ee94d0bfb9a234ddc4627da5ba42f53ba9b9fa18
sunlife3 <43718880+sunlife3@users.noreply.github.com>
2022-05-03 00:32:40 +0900

    until　GetInfo
    got file information of kernel file using GetInfo() function

--- Makefile
+++ Makefile
@@ -1,11 +1,24 @@
 build: main.c efi.h
 	clang -target x86_64-pc-win32-coff \
 		  -fno-stack-protector -fshort-wchar \
+		  -nostdlibinc\
 		  -mno-red-zone -Wall\
 		  -c main.c
-	lld-link /subsystem:efi_application /entry:EfiMain /out:main.efi main.o
+	lld-link -subsystem:efi_application -nodefaultlib -dll\
+			 -entry:EfiMain -out:main.efi main.o
 	mv main.efi fs/EFI/BOOT/BOOTX64.EFI
 
+buildgcc: main.c efi.h
+	x86_64-w64-mingw32-gcc -Wall -Wextra -e EfiMain -nostdinc -nostdlib \
+    -fno-builtin -fno-common -Wl,--subsystem,10 -o main.efi main.c 
+	mv main.efi fs/EFI/BOOT/BOOTX64.EFI
+
+kernel: kernel.cpp
+	clang++ -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone \
+	        -fno-exceptions -fno-rtti -std=c++17 -c kernel.cpp
+	ld.lld --entry KernelMain -z norelro --image-base 0x100000 --static \
+	       -o kernel.elf kernel.o
+	mv kernel.elf fs/
 
 qemu: OVMF.fd fs/EFI/BOOT/BOOTX64.EFI
 	qemu-system-x86_64 -bios OVMF.fd -hda fat:rw:fs
--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 
 #include "efi.h"
 #include "debug.h"
-#include "memorymap.h"
+#include "common.h"
 
 struct EFI_SYSTEM_TABLE *ST;
 struct EFI_BOOT_SERVICES *BS;
-EFI_GUID gEfiBlockIoProtocolGuid;
-EFI_GUID gEfiLoadedImageProtocolGuid;
 
 void Halt(void){
     while (1) __asm__("hlt");
@@ -43,7 +41,7 @@ EFI_STATUS OpenRootDir(EFI_HANDLE ImageHandle, struct EFI_FILE_PROTOCOL** root){
         ST->ConOut->OutputString(ST->ConOut,msg);
         return status;
     }
-    ST->ConOut->OutputString(ST->ConOut, L"OpenProtocol2\r\n");
+    
     status = ST->BootServices->OpenProtocol(
         loaded_image->DeviceHandle,
         &gEfiSimpleFileSystemProtocolGuid,
@@ -57,8 +55,20 @@ EFI_STATUS OpenRootDir(EFI_HANDLE ImageHandle, struct EFI_FILE_PROTOCOL** root){
         ST->ConOut->OutputString(ST->ConOut,msg);
         return status;
     }
-    ST->ConOut->OutputString(ST->ConOut, L"OpenRootDir3 done\n");
     return fs->OpenVolume(fs, root);
+}
+
+EFI_STATUS SaveMemoryMap(struct MemoryMap* map, struct EFI_FILE_PROTOCOL* file){
+    EFI_STATUS status=0;
+    //CHAR8 buf[256];
+    UINTN len;
+
+    CHAR8* header = "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
+    len = charStrLen (header);
+    file -> Write(file, &len, header);
+    //ST->ConOut->OutputString(ST->ConOut,L"written");
+    // TO BE IMPLEMENTED :: SAVE MEMORYMAP (referense osbook02b)
+    return status;
 }
 
 CHAR8 memmap_buf[4096*4];
@@ -70,20 +80,78 @@ EFI_STATUS EfiMain (EFI_HANDLE ImageHandle,
     BS = SystemTable->BootServices;
     
     SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
-    //SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello UEFI!\n");
-    ST->ConOut->OutputString(ST->ConOut, L"Hello UEFI!\n");
+    //ST->ConOut->OutputString(ST->ConOut, L"Hello UEFI!\n");
 
     struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
     status = GetMemoryMap(&memmap,SystemTable);
     if(status){
-        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Failed to open root directory\n");
+        ST->ConOut->OutputString(ST->ConOut, L"Failed to get memory map\r\n");
         Halt();
     }
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"succcess open root directory\r\n");
     
     struct EFI_FILE_PROTOCOL* root_dir;
     status = OpenRootDir(ImageHandle, &root_dir);
+    if(status){
+        retErrMsg(status);
+        ST->ConOut->OutputString(ST->ConOut, L"Failed to open root directory\n");
+        ST->ConOut->OutputString(ST->ConOut, msg);
+        Halt();
+    }
+
+    struct EFI_FILE_PROTOCOL* memmap_file;
+    status = root_dir->Open(
+      root_dir, &memmap_file, L"\\memmap",
+      EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
+    if(status){
+        retErrMsg(status);
+        ST->ConOut->OutputString(ST->ConOut, L"Failed to open '\\memmap'\n");
+        ST->ConOut->OutputString(ST->ConOut, msg);
+        Halt();
+    }
+    SaveMemoryMap(&memmap, memmap_file);
+    status = memmap_file->Close(memmap_file);
+    //ST->ConOut->OutputString(ST->ConOut, L"CLOSED");
     
+    //loading kernel
+    struct EFI_FILE_PROTOCOL* kernel_file;
+    status = root_dir->Open(
+        root_dir, &kernel_file, L"kernel.elf" , EFI_FILE_MODE_READ, 0);
+    if(status){
+        retErrMsg(status);
+        ST->ConOut->OutputString(ST->ConOut, L"Error at root_dir->Open\n");
+        ST->ConOut->OutputString(ST->ConOut, msg);
+        Halt();
+    }    
+    UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
+    UINT8 file_info_buffer[file_info_size];
+    status = kernel_file->GetInfo(kernel_file, &gEfiFileInfoGuid, &file_info_size, file_info_buffer);
+    if(status){
+        retErrMsg(status);
+        ST->ConOut->OutputString(ST->ConOut, L"at GetInfo\n");
+        ST->ConOut->OutputString(ST->ConOut, msg);
+        Halt();
+    }
+
+    //EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
+    //UINTN kernel_file_size = file_info->FileSize;
+    //EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
+    //BS -> AllocatePages(AllocateAddress, EfiLoaderData, (kernel_file_size + 0xfff)/0x1000, &kernel_base_addr);
+    if(status){
+        retErrMsg(status);
+        ST->ConOut->OutputString(ST->ConOut, L"at AllocatePages\n");
+        ST->ConOut->OutputString(ST->ConOut, msg);
+        Halt();
+    }
+    //kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
+    //ST->ConOut->OutputString(ST->ConOut, L"at \n");
+    if(status){
+        retErrMsg(status);
+        ST->ConOut->OutputString(ST->ConOut, L"at Read\n");
+        ST->ConOut->OutputString(ST->ConOut, msg);
+        Halt();
+    }
+
+    ST->ConOut->OutputString(ST->ConOut,L"DONE");
 
     while(1);
     return EFI_SUCCESS;
